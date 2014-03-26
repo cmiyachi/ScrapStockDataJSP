447734367e5198f065f00d7451ca4142b6efee18
zyanguml <i990802b@yahoo.com>
2014-03-25 20:51:46 -0400

    This is just a test to check that ant enviromnet is setup correctly.

---
+++ src/test/business/DOATest.java
@@ -0,0 +1,38 @@
+package test.business; 
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+/**
+* ScrapStockData Tester.
+ * This test program unit tests the class ScrapStockData -
+* 
+* @author cmiyachi
+* @since <pre>Feb 13, 2014</pre> 
+* @version 1.0 
+*/ 
+public class DOATest {
+
+
+	@Before
+	public void before() throws Exception {
+	} 
+	@After
+	public void after() throws Exception {
+	} 
+
+       @Test
+       public void deadOrAliveTest() {
+	  assertNotNull( 1); 
+
+       }
+
+
+} 
